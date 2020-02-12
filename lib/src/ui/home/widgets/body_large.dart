6507a2170e0e597f496500eb7a5ab01ef3cf42ff
samuelematias <hi@samuelematias.com>
2020-02-12 17:52:24 -0300

    update body's

--- lib/src/ui/home/widgets/body_large.dart
+++ lib/src/ui/home/widgets/body_large.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_web_github_pages/src/core/constants/app_constants.dart';
+import 'package:flutter_web_github_pages/src/core/util/browser/browser.dart';
 
 class BodyLarge extends StatelessWidget {
   @override
@@ -12,12 +15,25 @@ class BodyLarge extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'Github Pages LARGE!',
-                style: TextStyle(
-                  color: Colors.red,
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  'A Example of Flutter Web project Hosted on Github Pages and using Github Actions for CI/CD.',
+                  style: TextStyle(
+                    color: Colors.black87,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              )
+              ),
+              GestureDetector(
+                onTap: () => ExternalBrowser.open(
+                    context: context, url: AppConstants.GITHUB_REPO_LINK),
+                child: Icon(
+                  AntDesign.github,
+                  color: Colors.black87,
+                  size: 100,
+                ),
+              ),
             ],
           ),
         ),
--- lib/src/ui/home/widgets/body_small.dart
+++ lib/src/ui/home/widgets/body_small.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_web_github_pages/src/core/constants/app_constants.dart';
+import 'package:flutter_web_github_pages/src/core/util/browser/browser.dart';
 
 class BodySmall extends StatelessWidget {
   @override
@@ -16,12 +19,25 @@ class BodySmall extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Text(
-                    'Github Pages SMALL!',
-                    style: TextStyle(
-                      color: Colors.blue,
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+                    child: Text(
+                      'A Example of Flutter Web project Hosted on Github Pages and using Github Actions for CI/CD.',
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
+                  ),
+                  GestureDetector(
+                    onTap: () => ExternalBrowser.open(
+                        context: context, url: AppConstants.GITHUB_REPO_LINK),
+                    child: Icon(
+                      AntDesign.github,
+                      color: Colors.black87,
+                      size: 100,
+                    ),
                   )
                 ],
               ),
