4b5dfe7e2a0e88e8f9fc0d64d9b5c04b95d8d73f
samuelematias <hi@samuelematias.com>
2020-02-12 17:35:30 -0300

    create the router

---
+++ lib/src/core/routes/router.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_github_pages/src/ui/home/home_page.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+        break;
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}.'),
+            ),
+          ),
+        );
+    }
+  }
+}
